fea9a8dd23a03363eaf262147283822de064c363
Paul Henrion <phenrion@genymobile.com>
2019-02-13 15:17:07 +0100

    Fix log error; moved down

--- app.js
+++ app.js
@@ -26,13 +26,13 @@ app.post('/motion-stories-bugs-to-slack', function(req, res) {
     
   let sprintChanged = changelog.items.find(item => item.field === "Sprint")
   let status = changelog.items.find(item => item.field === "status")
-  console.log(`status == ${status}`)
   let isDone = !!status && status.toString === "Done"
   let addedToActiveSprint = sprintChangedToActiveSprint(issue.fields.customfield_10004)
 
   if (!sprintChanged) {
 
     console.log('No Sprint change')
+    console.log(`status == ${status.toString}`)
 
     if (isDone) {
       let msg = `${user.displayName} marked ${issue.key} as ${issue.fields.status.name}`
