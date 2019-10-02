e2de2ab8f85d5f5936644343886dda0506155534
Paul Henrion <phenrion@genymobile.com>
2019-10-02 16:06:20 +0200

    Debug mode ON (bis)
    fix type

--- app.js
+++ app.js
@@ -25,8 +25,8 @@ app.post('/motion-stories-bugs-to-slack', function(req, res) {
   // let SLACK_URL_MOTION_TESTING = process.env.SLACK_URL_CM_MOTION_TESTING
 
   // DEBUG
-  SLACK_URL_MOTION = process.env.SLACK_URL_DM_PAUL
-  SLACK_URL_MOTION_TESTING = process.env.SLACK_URL_DM_PAUL
+  let SLACK_URL_MOTION = process.env.SLACK_URL_DM_PAUL
+  let SLACK_URL_MOTION_TESTING = process.env.SLACK_URL_DM_PAUL
   
   let sprintChanged = !!changelog ? changelog.items.find(item => item.field === "Sprint") : null
   let status = !!changelog ? changelog.items.find(item => item.field === "status") : null
